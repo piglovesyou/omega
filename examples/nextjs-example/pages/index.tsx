f125c31bbfa3055562c77d17e661dc16a9c81546
Soichi Takamura <thepiglovesyou@gmail.com>
2020-09-27 19:36:59 +0900

    wip: 3 forms in tab contents

--- examples/nextjs-example/pages/index.tsx
+++ examples/nextjs-example/pages/index.tsx
@@ -1,7 +1,16 @@
 import Head from 'next/head';
-import { FormComponent } from '../__generated__/user-account/form';
+import { useCallback, useState } from 'react';
+import { FormComponent as SimpleFormComponent } from '../__generated__/simple-example-app/form';
+import { FormComponent as ComplexFormComponent } from '../__generated__/complex-example-app/form';
+import { FormComponent as AllTypesFormComponent } from '../__generated__/all-types-example-app/form';
 
 export default function Application() {
+  const [selectedIndex, setSelectedIndex] = useState(2);
+  const onSubmit = useCallback(
+    (values) =>
+      alert(`👍\n\nYou posted:\n${JSON.stringify(values, undefined, 2)}`),
+    [],
+  );
   return (
     <div className="my-form">
       <Head>
@@ -12,11 +21,38 @@ export default function Application() {
         />
       </Head>
       <h1>My form</h1>
-      <FormComponent
-        onSubmit={(values) =>
-          alert(`👍\n\nYou posted:\n${JSON.stringify(values, undefined, 2)}`)
-        }
-      />
+
+      {selectedIndex === 0 && <SimpleFormComponent onSubmit={onSubmit} />}
+      {selectedIndex === 1 && <ComplexFormComponent onSubmit={onSubmit} />}
+      {selectedIndex === 2 && <AllTypesFormComponent onSubmit={onSubmit} />}
+
+      <div className="omega-form-tab">
+        <button
+          className={`omega-form-tab-item ${
+            selectedIndex === 0 ? 'omega-form-tab-item--selected' : ''
+          }`}
+          onClick={() => setSelectedIndex(0)}
+        >
+          Simple example
+        </button>
+        <button
+          className={`omega-form-tab-item ${
+            selectedIndex === 1 ? 'omega-form-tab-item--selected' : ''
+          }`}
+          onClick={() => setSelectedIndex(1)}
+        >
+          Complex example
+        </button>
+        <button
+          className={`omega-form-tab-item ${
+            selectedIndex === 2 ? 'omega-form-tab-item--selected' : ''
+          }`}
+          onClick={() => setSelectedIndex(2)}
+        >
+          All field types example
+        </button>
+      </div>
+
       <hr />
       <p className="my-trailing-text">
         This is a demonstration of{' '}
